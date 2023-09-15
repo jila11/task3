2eb2106180e5b892cd3bffde6c50a05ec8932be6
Jila-Rasnat <jilarasnat.balachandar@verticurl.com>
2023-09-15 09:19:39 +0530

    first commit

---
+++ src/main/java/com/task3/jenkins_task3/JenkinsTask3Application.java
@@ -0,0 +1,13 @@
+package com.task3.jenkins_task3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsTask3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsTask3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/task3/jenkins_task3/JenkinsTask3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task3.jenkins_task3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsTask3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
